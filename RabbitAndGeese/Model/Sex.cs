f2c611c3658df7876acc5932a299f360da807b11
Maken Hubbard <makhub26@outlook.com>
2018-09-17 21:24:40 -0500

    adding code for controllers and some coverage on linq

---
+++ RabbitAndGeese/Model/Sex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RabbitAndGeese.Model
+{
+    public enum Sex
+    {
+        Male, Female
+    }
+}
